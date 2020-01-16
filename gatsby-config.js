39ac86bcfc5475f7bb4e15b60b6d1ddf617b37a3
shanejonas <jonas.shane@gmail.com>
2020-01-16 13:31:09 -0800

    fix: build config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/pristine-typescript-gatsby-react-material-ui",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pristine Gatsby Default Starter`,
+    description: ``,
+    author: ``,
   },
   plugins: [
     "gatsby-plugin-material-ui",
